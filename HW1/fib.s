52b6d7810b73484a0f01bc820110410ec5463224
Nick Winans <nw5zp@virginia.edu>
2020-01-29 18:34:21 -0500

    fixed fib bounds

--- HW1/fib.s
+++ HW1/fib.s
@@ -1,3 +1,8 @@
+.text
+
+main:
+li $4, 5
+
 Fib:
 li $2, 0            # load zero into the return register
 slti $18, $4, 1     # if (n < 1) tmp = 1
@@ -7,8 +12,8 @@ li $17, 1           # b = 0
 add $2, $16, $17    # c = a + b 
 
 Loop:
-li $18, 1           # tmp = 1
-beq $4, $18, Exit   # if (n == 1) goto Exit
+li $18, 0           # tmp = 0
+beq $4, $18, Exit   # if (n == 0) goto Exit
 add $2 $16, $17     # c = a + b
 move $16, $17       # a = b
 move $17, $2        # b = c
